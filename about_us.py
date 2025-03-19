5144d2cf3504101a367f791cc2b3616b653cf956
The Hidden Prospects <carlosjericodelatorre@gmail.com>
2025-03-20 00:00:46 +0800

    updated the about_us.py, renamed 'about_us' img folder > 'original_pics', added new image folder 'about_us'

--- about_us.py
+++ about_us.py
@@ -9,22 +9,22 @@ st.write("---")
 
 # Group Members List
 group_members = [
-    {"name": "Dela Mata, Katrina Cassandra P.", "image": r"static\images\about_us\DELA MATA.jpg", "facebook": "https://www.facebook.com/share/1E4Nd41pQc/", "linkedin": "#"},
-    {"name": "Dominguez, Brein Lebron M.", "image": r"static\images\about_us\DOMINGUEZ.jpeg", "facebook": "https://www.facebook.com/share/191JYgG1qo/", "linkedin": "#"},
-    {"name": "Eboña, Kyla L.", "image": r"static\images\about_us\EBOÑA.jpeg", "facebook": "https://www.facebook.com/share/15aucJZkXs/", "linkedin": "#"},
-    {"name": "Echano, Samantha Shane V.", "image": r"static\images\about_us\ECHANO.jpeg", "facebook": "https://www.facebook.com/share/15YnWcdq13/", "linkedin": "#"},
-    {"name": "Espanto, Ryza Regina A.", "image": r"static\images\about_us\ESPANTO.jpeg", "facebook": "https://www.facebook.com/share/17eJ51YbS1/", "linkedin": "#"},
-    {"name": "Estilles, Necamae B.", "image": r"static\images\about_us\ESTILLES.jpg", "facebook": "https://www.facebook.com/share/1A2JYf7MPK/", "linkedin": "#"},
-    {"name": "Francisco, Dan Angelo R.", "image": r"static\images\about_us\FRANCISCO.jpeg", "facebook": "https://www.facebook.com/share/1ALefGwrr7/", "linkedin": "#"},
-    {"name": "Gaya, Princess Jannah V.", "image": r"static/images/about_us/GAYA.jpeg", "facebook": "https://www.facebook.com/share/163xwL7fDN/", "linkedin": "#"},
-    {"name": "Gomez, Daphne A.", "image": r"static\images\about_us\GOMEZ.jpeg", "facebook": "https://www.facebook.com/share/1FASbk6sxM/", "linkedin": "#"},
-    {"name": "Goyal, Ashley R.", "image": r"static\images\about_us\GOYAL.JPG", "facebook": "https://www.facebook.com/share/1DkJv4Cdt9/", "linkedin": "#"},
+    {"name": "Dela Mata,  \nKatrina Cassandra P.", "image": r"static/images/about_us/dela_mata.png", "facebook": "https://www.facebook.com/share/1E4Nd41pQc/", "linkedin": "#"},
+    {"name": "Dominguez,  \nBrein Lebron M.", "image": r"static\images\about_us\dominguez.png", "facebook": "https://www.facebook.com/share/191JYgG1qo/", "linkedin": "#"},
+    {"name": "Eboña,  \nKyla L.", "image": r"static\images\about_us\eboña.png", "facebook": "https://www.facebook.com/share/15aucJZkXs/", "linkedin": "#"},
+    {"name": "Echano,  \nSamantha Shane V.", "image": r"static\images\about_us\echano.png", "facebook": "https://www.facebook.com/share/15YnWcdq13/", "linkedin": "#"},
+    {"name": "Espanto,  \nRyza Regina A.", "image": r"static\images\about_us\espanto.png", "facebook": "https://www.facebook.com/share/17eJ51YbS1/", "linkedin": "#"},
+    {"name": "Estilles,  \nNecamae B.", "image": r"static\images\about_us\estilles.png", "facebook": "https://www.facebook.com/share/1A2JYf7MPK/", "linkedin": "#"},
+    {"name": "Francisco,  \nDan Angelo R.", "image": r"static\images\about_us\francisco.png", "facebook": "https://www.facebook.com/share/1ALefGwrr7/", "linkedin": "#"},
+    {"name": "Gaya,  \nPrincess Jannah V.", "image": r"static\images\about_us\gaya.png", "facebook": "https://www.facebook.com/share/163xwL7fDN/", "linkedin": "#"},
+    {"name": "Gomez,  \nDaphne A.", "image": r"static\images\about_us\gomez.png", "facebook": "https://www.facebook.com/share/1FASbk6sxM/", "linkedin": "#"},
+    {"name": "Goyal,  \nAshley R.", "image": r"static\images\about_us\goyal.png", "facebook": "https://www.facebook.com/share/1DkJv4Cdt9/", "linkedin": "#"},
 ]
 
 # Developers List
 developers = [
-    {"name": "John Paul Curada", "role": "Developer - J&G Innovations", "image": "https://via.placeholder.com/150", "facebook": "https://www.facebook.com/jp.curada.3", "linkedin": "https://www.linkedin.com/in/jpcurada/"},
-    {"name": "Carlos Jerico Dela Torre", "role": "Developer", "image": "https://via.placeholder.com/150", "facebook": "https://www.facebook.com/2iLiTE", "linkedin": "https://www.linkedin.com/in/delatorrecj/"}
+    {"name": "John Paul Curada", "role": "Developer - J&G Innovations", "image": r"static\images\about_us\curada.png", "facebook": "https://www.facebook.com/jp.curada.3", "linkedin": "https://www.linkedin.com/in/jpcurada/"},
+    {"name": "Carlos Jerico Dela Torre", "role": "Developer", "image": r"static\images\about_us\dela_torre.png", "facebook": "https://www.facebook.com/2iLiTE", "linkedin": "https://www.linkedin.com/in/delatorrecj/"}
 ]
 
 def display_members(members, show_linkedin=False):
@@ -39,7 +39,7 @@ def display_members(members, show_linkedin=False):
             
             with col1:
                 # Display member image
-                st.image(member["image"], width=80)
+                st.image(member["image"], width=10000)
             
             with col2:
                 # Display name
@@ -51,9 +51,7 @@ def display_members(members, show_linkedin=False):
                 
                 # Social media links with HTML to control image size
                 st.markdown(f"""
-                <a href="{member['facebook']}" target="_blank">
-                    <img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" width="20">
-                </a>
+                <a href="{member['facebook']}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" width="20"></a>
                 {f'<a href="{member["linkedin"]}" target="_blank"><img src="https://cdn-icons-png.flaticon.com/512/174/174857.png" width="20"></a>' if show_linkedin and "linkedin" in member and member["linkedin"] != "#" else ""}
                 """, unsafe_allow_html=True)
             
